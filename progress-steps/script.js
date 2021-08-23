ed71e62489a763ca80ffe8a4d5464f74887df155
cyq7on <cyq7on@qq.com>
2021-08-23 21:24:55 +0800

    完成第二个项目，progress-steps

---
+++ progress-steps/script.js
@@ -0,0 +1,46 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update();
+})
+
+prev.addEventListener('click', () => {
+    currentActive--;
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
+})
+
+function update() {
+    // circle状态控制
+    circles.forEach((circle, index) => {
+        if (currentActive < index + 1) {
+            circle.classList.remove('active');
+        } else {
+            circle.classList.add('active');
+        }
+    })
+
+    // 按钮状态控制
+    if(currentActive <= 1) {
+        prev.disabled = true;
+    }else if(currentActive >= circles.length) {
+        next.disabled = true;
+    }else {
+        next.disabled = false;
+        prev.disabled = false;
+    }
+
+    // 设置进度条长度
+    progress.style.width = (currentActive - 1) * 100 / (circles.length - 1) + '%';
+}
